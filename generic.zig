72e54cceea918a8969544f6c9b5baf21c7c66005
Jonathan Marler <johnnymarler@gmail.com>
2023-09-20 10:15:10 -0600

    explore generic type pass

---
+++ generic.zig
@@ -0,0 +1,17 @@
+
+
+pub const Contract = struct {
+};
+pub const TypeContract = struct {
+};
+pub const TypeInfoContract = struct {
+};
+
+const AST = struct { };
+pub fn analyze(function: AST) []TypeContract {
+    @panic("todo");
+}
+
+//test analyze {
+//    analyze
+//}
